fe0c46c0f8ff8cd4087a557e4075bdf937d66fe1
jafers <jafers@erau.edu>
2015-03-01 21:13:26 -0500

    Nothing much.

--- localRepo/src/Application.java
+++ localRepo/src/Application.java
@@ -6,7 +6,8 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello"); //display message
+		System.out.println("Hello"); //display message  
+		
 
 	}
 
